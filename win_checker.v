76e7418f555392034aaa17b2d6c7a0aea8083f09
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2024-06-04 17:07:50 -0700

    fixed syntax and logical error in win checker

--- win_checker.v
+++ win_checker.v
@@ -1,28 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:56:56 03/19/2013 
-// Design Name: 
-// Module Name:    win_checker 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 module win_checker(
-    input wire [49:0] board_bit; // Your 50-bit number
+    input wire [49:0] board_bit, // Your 50-bit number
     input wire [1:0] player_id,
-    output wire player_won,
+    output wire player_won
 );
     reg win;
     integer i, j;
@@ -39,33 +20,46 @@ module win_checker(
 
     // Given player value and game board, determine if that player has won the game
     always @(*) begin
+        win = 0;
         // Check rows and columns
         for (i = 0; i < 5; i = i + 1) begin
-            if (board[i][0] == PLAYER_ID && board[i][1] == PLAYER_ID && board[i][2] == PLAYER_ID && board[i][3] == PLAYER_ID) begin
+            if (board[i][0] == player_id && board[i][1] == player_id && board[i][2] == player_id && board[i][3] == player_id) begin
                 win = 1;
             end
-            if (board[i][1] == PLAYER_ID && board[i][2] == PLAYER_ID && board[i][3] == PLAYER_ID && board[i][4] == PLAYER_ID) begin
+            if (board[i][1] == player_id && board[i][2] == player_id && board[i][3] == player_id && board[i][4] == player_id) begin
                 win = 1;
             end
-            if (board[0][i] == PLAYER_ID && board[1][i] == PLAYER_ID && board[2][i] == PLAYER_ID && board[3][i] == PLAYER_ID) begin
+            if (board[0][i] == player_id && board[1][i] == player_id && board[2][i] == player_id && board[3][i] == player_id) begin
                 win = 1;
             end
-            if (board[1][i] == PLAYER_ID && board[2][i] == PLAYER_ID && board[3][i] == PLAYER_ID && board[4][i] == PLAYER_ID) begin
+            if (board[1][i] == player_id && board[2][i] == player_id && board[3][i] == player_id && board[4][i] == player_id) begin
                 win = 1;
             end
         end
 
         // Check diagonals
-        if (board[0][0] == PLAYER_ID && board[1][1] == PLAYER_ID && board[2][2] == PLAYER_ID && board[3][3] == PLAYER_ID) begin
+        if (board[0][0] == player_id && board[1][1] == player_id && board[2][2] == player_id && board[3][3] == player_id) begin
             win = 1;
         end
-        if (board[1][1] == PLAYER_ID && board[2][2] == PLAYER_ID && board[3][3] == PLAYER_ID && board[4][4] == PLAYER_ID) begin
+        if (board[1][1] == player_id && board[2][2] == player_id && board[3][3] == player_id && board[4][4] == player_id) begin
             win = 1;
         end
-        if (board[0][1] == PLAYER_ID && board[1, 2] == PLAYER_ID && board[2][3] == PLAYER_ID && board[3][4] == PLAYER_ID) begin
+        if (board[0][1] == player_id && board[1][2] == player_id && board[2][3] == player_id && board[3][4] == player_id) begin
             win = 1;
         end
-        if (board[1][0] == PLAYER_ID && board[2][1] == PLAYER_ID && board[3][2] == PLAYER_ID && board[4][3] == PLAYER_ID) begin
+        if (board[1][0] == player_id && board[2][1] == player_id && board[3][2] == player_id && board[4][3] == player_id) begin
+            win = 1;
+        end
+        if (board[4][0] == player_id && board[3][1] == player_id && board[2][2] == player_id && board[1][3] == player_id) begin
+            win = 1;
+        end
+        if (board[3][1] == player_id && board[2][2] == player_id && board[1][3] == player_id && board[0][4] == player_id) begin
+            win = 1;
+        end
+        if (board[3][0] == player_id && board[2][1] == player_id && board[1][2] == player_id && board[0][3] == player_id) begin
+            win = 1;
+        end
+        if (board[4][1] == player_id && board[3][2] == player_id && board[2][3] == player_id && board[1][4] == player_id) begin
             win = 1;
         end
     end
